614203d10b21e8992d7ee5a5581c4c198136f0fa
Jason Koi <lnakhoi1811@gmail.com>
2022-03-27 15:08:10 +0700

    first commit

---
+++ FaceRecognition_GUI.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+from PIL import Image, ImageTk
+
+root= tk.Tk()
+
+# initialize canvas frame
+canvas=tk.Canvas(root,width=400,height=450)
+canvas.grid(columnspan=3)
+
+# initialize logo 
+
+root.mainloop()
